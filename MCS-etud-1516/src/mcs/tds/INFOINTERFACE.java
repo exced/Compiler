8b06b5114ff180c552241ef617264cca1ee54ac4
origin3 <thomas.barras26@gmail.com>
2016-06-05 16:25:18 +0200

    UML

--- MCS-etud-1516/src/mcs/tds/INFOINTERFACE.java
+++ MCS-etud-1516/src/mcs/tds/INFOINTERFACE.java
@@ -1,5 +1,20 @@
 package mcs.tds;
 
-public class INFOINTERFACE {
+import mcs.type.DTYPE;
+
+public class INFOINTERFACE extends INFO {
+
+	protected TDS tdsContenu;
+
+	public INFOINTERFACE(DTYPE t, TDS tds) {
+		super(t);	
+		tdsContenu = tds;
+	}
+
+	public TDS getContenu() { return tdsContenu; }
+
+	public String toString() {
+		return "INFOINTERFACE : contenu => " + tdsContenu;
+	}
 
 }
